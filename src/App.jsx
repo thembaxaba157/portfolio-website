3bf49393ba288493eab4f7916dbd45042b9a1ef6
thembaxaba157 <thembaxaba157@gmail.com>
2025-02-20 20:13:29 +0200

    scrap it

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 // import './App.css'
 import styles from "./App.module.css";
-import { Navbar } from "./components/Navbar/Navbar";
+
 
 function App() {
 
   return <div className={styles.App}>
     
-    <Navbar/>
+    
     
     </div>
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-import "@fontsource/outfit"
-import "@fontsource/roboto"
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
