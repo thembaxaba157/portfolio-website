2faba78ec9f739b4b3c2ccd8c6aebfa5eaff7e65
thembaxaba157 <thembaxaba157@gmail.com>
2025-02-20 13:33:21 +0200

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import './App.css'
+import styles from "./App.module.css";
+import { Navbar } from "./components/Navbar/Navbar";
+
+function App() {
+
+  return <div className={styles.App}>
+    
+    <Navbar/>
+    
+    </div>
+
+}
+
+export default App
