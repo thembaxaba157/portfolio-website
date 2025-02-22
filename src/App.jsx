6b28d6ba873fbfafc1630c3d3e76b28d9f39b65f
thembaxaba157 <thembaxaba157@gmail.com>
2025-02-22 12:10:01 +0200

    Unfinished Home component with all elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import './App.css'
 import Header from './components/header/Header'
+import Home from './components/home/Home'
+
 
 
 function App() {
 
   return (
-    <><Header>
-      </Header></>
+    <>
+      <Header />
+      
+      
+      <main className='main'>
+        <Home />
+        
+      </main>
+      
+      </>
   )
 
 }
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://github.com/thembaxaba157"
+        className="home__social-icon" target="_blank">
+        <i className="uil uil-github-alt"></i>
+        </a>
+
+        <a href="https://www.linkedin.com/in/themba-xaba-3740a9261/"
+        className="home__social-icon" target="_blank">
+        <i className="uil uil-linkedin-alt"></i>
+        </a>
+
+        <a href="https://wa.me/27822837306"
+        className="home__social-icon" target="_blank">
+        <i className="uil uil-whatsapp"></i>
+        </a>
+
+
+    </div>
+  )
+}
+
+export default Social
