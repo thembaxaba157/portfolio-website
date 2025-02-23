7b670873942ad0e8ee6b1ea73d9b161a36032c8c
thembaxaba157 <thembaxaba157@gmail.com>
2025-02-23 22:05:47 +0200

    Skills section Complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import Header from './components/header/Header'
 import Home from './components/home/Home'
 import About from './components/about/About'
-
+import Skills from './components/skills/Skills'
 
 function App() {
 
@@ -14,6 +14,7 @@ function App() {
       <main className='main'>
         <Home />
         <About />
+        <Skills/>
       </main>
       
       </>
