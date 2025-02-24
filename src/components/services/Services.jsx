9118a014af6750c273f7af848aa61cae226dc67f
thembaxaba157 <thembaxaba157@gmail.com>
2025-02-24 14:58:38 +0200

    push due to loadshedding

---
+++ src/components/services/Services.jsx
@@ -0,0 +1,195 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <section className="services section" id="services">
+    <h2 className="section__title">Services</h2>
+    <span className="section__subtitle">What I offer</span>
+
+    <div className="services__container container grid">
+        <div className="services__content">
+            <i className="uil uil services__icon"></i>
+            <h3 className="services__title"></h3>
+
+
+            <span className="services__button">View More{" "} <i
+        className="uil uil-arrow-right services__button-icon"></i></span>
+
+        <div className="services__modal">
+            <div className="services__modal-content">
+                <i className="uil uil-times services__modal-close"></i>
+
+                <h3 className="services__modal-title"></h3>
+                <p className="services__modal-description">
+                I build scalable, high-performance web applications using modern technologies. 
+                From backend logic to user interfaces, I ensure smooth integration and efficiency.
+                </p>
+
+                <ul className="services__modal-services grid">
+                    <li className="services__modal-service">
+                        <i className="uil uil-check-circle service__modal-icon">
+                        </i>
+                        <p className="services__modal-info">
+                        Full-stack development using Java (Spring Boot) & React
+                        </p>
+                    </li>
+
+                    <li className="services__modal-service">
+                        <i className="uil uil-check-circle service__modal-icon">
+                        </i>
+                        <p className="services__modal-info">
+                        Scalable REST API design & development</p>
+                    </li>
+
+                    <li className="services__modal-service">
+                        <i className="uil uil-check-circle service__modal-icon">
+                        </i>
+                        <p className="services__modal-info">
+                     Database management with MySQL
+                        </p>
+                    </li>
+
+                    <li className="services__modal-service">
+                        <i className="uil uil-check-circle service__modal-icon">
+                        </i>
+                        <p className="services__modal-info">
+                     NOTE: I am not limited to these technologies/Tech Stack only, Contact For More Info
+                        </p>
+                    </li>
+
+
+
+                </ul>
+
+            </div>
+        </div>
+
+
+        </div>
+        
+        <div className="services__content">
+            <i className="uil uil-web-grid services__icon"></i>
+            <h3 className="services__title">Web Application <br /> Development</h3>
+
+
+            <span className="services__button">View More{" "} <i
+        className="uil uil-arrow-right services__button-icon"></i></span>
+
+        <div className="services__modal">
+            <div className="services__modal-content">
+                <i className="uil uil-times services__modal-close"></i>
+
+                <h3 className="services__modal-title"></h3>
+                <p className="services__modal-description">
+                I build scalable, high-performance web applications using modern technologies. 
+                From backend logic to user interfaces, I ensure smooth integration and efficiency.
+                </p>
+
+                <ul className="services__modal-services grid">
+                    <li className="services__modal-service">
+                        <i className="uil uil-check-circle service__modal-icon">
+                        </i>
+                        <p className="services__modal-info">
+                        Full-stack development using Java (Spring Boot) & React
+                        </p>
+                    </li>
+
+                    <li className="services__modal-service">
+                        <i className="uil uil-check-circle service__modal-icon">
+                        </i>
+                        <p className="services__modal-info">
+                        Scalable REST API design & development</p>
+                    </li>
+
+                    <li className="services__modal-service">
+                        <i className="uil uil-check-circle service__modal-icon">
+                        </i>
+                        <p className="services__modal-info">
+                     Database management with MySQL
+                        </p>
+                    </li>
+
+                    <li className="services__modal-service">
+                        <i className="uil uil-check-circle service__modal-icon">
+                        </i>
+                        <p className="services__modal-info">
+                     NOTE: I am not limited to these technologies/Tech Stack only, Contact For More Info
+                        </p>
+                    </li>
+
+
+
+                </ul>
+
+            </div>
+        </div>
+
+
+        </div>
+
+        <div className="services__content">
+            <i className="uil uil services__icon"></i>
+            <h3 className="services__title"></h3>
+
+
+            <span className="services__button">View More{" "} <i
+        className="uil uil-arrow-right services__button-icon"></i></span>
+
+        <div className="services__modal">
+            <div className="services__modal-content">
+                <i className="uil uil-times services__modal-close"></i>
+
+                <h3 className="services__modal-title"></h3>
+                <p className="services__modal-description">
+                I build scalable, high-performance web applications using modern technologies. 
+                From backend logic to user interfaces, I ensure smooth integration and efficiency.
+                </p>
+
+                <ul className="services__modal-services grid">
+                    <li className="services__modal-service">
+                        <i className="uil uil-check-circle service__modal-icon">
+                        </i>
+                        <p className="services__modal-info">
+                        Full-stack development using Java (Spring Boot) & React
+                        </p>
+                    </li>
+
+                    <li className="services__modal-service">
+                        <i className="uil uil-check-circle service__modal-icon">
+                        </i>
+                        <p className="services__modal-info">
+                        Scalable REST API design & development</p>
+                    </li>
+
+                    <li className="services__modal-service">
+                        <i className="uil uil-check-circle service__modal-icon">
+                        </i>
+                        <p className="services__modal-info">
+                     Database management with MySQL
+                        </p>
+                    </li>
+
+                    <li className="services__modal-service">
+                        <i className="uil uil-check-circle service__modal-icon">
+                        </i>
+                        <p className="services__modal-info">
+                     NOTE: I am not limited to these technologies/Tech Stack only, Contact For More Info
+                        </p>
+                    </li>
+
+
+
+                </ul>
+
+            </div>
+        </div>
+
+
+        </div>
+
+    </div>
+    </section>
+  )
+}
+
+export default Services
