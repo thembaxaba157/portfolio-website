e6df7ba855dc89266bd80f884364a5c8a243017b
thembaxaba157 <thembaxaba157@gmail.com>
2025-02-26 16:40:57 +0200

    Qualification skeleton for jsx(incomplete)

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Home from './components/home/Home'
 import About from './components/about/About'
 import Skills from './components/skills/Skills'
 import Services from './components/services/Services'
+import Qualification from './components/qualification/Qualification'
+
 
 function App() {
 
@@ -17,6 +19,7 @@ function App() {
         <About />
         <Skills/>
         <Services/>
+        <Qualification/>
       </main>
       
       </>
