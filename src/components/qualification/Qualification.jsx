9de404ec68612bd4e05dbb7c221b67ff3d61bbfa
thembaxaba157 <thembaxaba157@gmail.com>
2025-02-27 11:39:49 +0200

    Added all data for qualifications

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -26,24 +26,119 @@ const Qualification = () => {
                 <div className="qualification__content">
                     <div className="qualification__data">
                        <div>
-                       <h3 className="qualification__title"></h3>
+                       <h3 className="qualification__title">Systems Development</h3>
+                        <span className="qualification__subitile">WeThinkCode_</span>
+                        <div className="qualification__calender">
+                            <i className="uil uil-calendar-alt"></i> 2022 - 2024(Complete)
+                        </div>
+                       </div>
+
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification__line"></span>
+                        </div>
+
+                    </div>
+
+
+                    <div className="qualification__data">
+
+                        <div></div>
+
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification__line"></span>
+                        </div>
+
+                       <div>
+                       <h3 className="qualification__title">BSc Computer Science{"(Incomplete)"}</h3>
                         <span className="qualification__subitile">University of Witwatersrand</span>
                         <div className="qualification__calender">
                             <i className="uil uil-calendar-alt"></i> 2018 - 2021(3rd year)
                         </div>
                        </div>
 
+                    </div>
+
+                    <div className="qualification__data">
+                       <div>
+                       <h3 className="qualification__title">National Senior Certificate</h3>
+                        <span className="qualification__subitile">Orlando West High</span>
+                        <div className="qualification__calender">
+                            <i className="uil uil-calendar-alt"></i> 2014 - 2017(Complete)
+                        </div>
+                       </div>
+
                         <div>
                             <span className="qualification__rounder"></span>
-                            <div className="qualification__line"></div>
+                            <span className="qualification__line"></span>
                         </div>
 
                     </div>
-
                     
 
 
                 </div>
+
+                <div className="qualification__content">
+                    <div className="qualification__data">
+                       <div>
+                       <h3 className="qualification__title">
+                       Jnr Software Engineer Intern</h3>
+                        <span className="qualification__subitile">MyDough</span>
+                        <div className="qualification__calender">
+                            <i className="uil uil-calendar-alt"></i> Feb 2024
+                        </div>
+                       </div>
+
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification__line"></span>
+                        </div>
+
+                    </div>
+
+
+                    <div className="qualification__data">
+
+                        <div></div>
+
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification__line"></span>
+                        </div>
+
+                       <div>
+                       <h3 className="qualification__title">BootCamp Technical Mentor</h3>
+                        <span className="qualification__subitile">WeThinkCode_</span>
+                        <div className="qualification__calender">
+                            <i className="uil uil-calendar-alt"></i> Apr 2023 - Aug 2023
+
+                        </div>
+                       </div>
+
+                    </div>
+
+                    <div className="qualification__data">
+                       <div>
+                       <h3 className="qualification__title">BootCamp Project Reviewer</h3>
+                        <span className="qualification__subitile">WeThinkCode_</span>
+                        <div className="qualification__calender">
+                            <i className="uil uil-calendar-alt"></i> Apr 2023 - Aug 2023
+                        </div>
+                       </div>
+
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification__line"></span>
+                        </div>
+
+                    </div>
+                    
+
+
+                </div>
+
             </div>
         </div>
     
