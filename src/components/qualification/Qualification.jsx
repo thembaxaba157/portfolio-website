05d3f55e6b286325121036a79dfab8243ea1d488
thembaxaba157 <thembaxaba157@gmail.com>
2025-02-27 16:03:15 +0200

    Added all data for qualifications

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -1,35 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./qualification.css"
 
 
 const Qualification = () => {
+
+    const [toggleState, setToggleState] = useState(1);
+    const toggleTab = (index) => {
+        setToggleState(index);
+    }
+
   return (
     <section className="qualification section">
-        <h2 className="section__title">Qualification</h2>
-        <span className="section__subtitle">My Personal Journey</span>
+        <h2 className="section__title">Journey</h2>
+        <span className="section__subtitle">My Personal Tech Journey</span>
     
         <div className="qualification__container container">
             <div className="qualification__tabs">
-                <div className="qualification__button
-                 qualification__active button--flex">
+                <div className={toggleState === 1 ? 
+                "qualification__button qualification__active button--flex" :
+                "qualification__button button--flex"}
+                onClick={() => toggleTab(1)}>
                     <i className="uil uil-graduation-cap
                      qualification__icon"></i>{" "}Education
                 </div>
 
-                <div className="qualification__button qualification__active button--flex">
+                <div className={toggleState === 2 ? 
+                "qualification__button qualification__active button--flex" :
+                "qualification__button button--flex"}
+                onClick={() => toggleTab(2)}>
                     <i className="uil uil-briefcase-alt
                      qualification__icon"></i>{" "}Experience
                 </div>
             </div>
             
             <div className="qualification__sections">
-                <div className="qualification__content">
+                <div className={toggleState === 1 ? "qualification__content qualification__content-active":
+                    "qualification__content"}>
                     <div className="qualification__data">
                        <div>
                        <h3 className="qualification__title">Systems Development</h3>
-                        <span className="qualification__subitile">WeThinkCode_</span>
-                        <div className="qualification__calender">
-                            <i className="uil uil-calendar-alt"></i> 2022 - 2024(Complete)
+                        <span className="qualification__subtitle">WeThinkCode_</span>
+                        <div className="qualification__calendar">
+                            <i className="uil uil-calendar-alt"></i> 2022 - 2024
                         </div>
                        </div>
 
@@ -52,8 +64,8 @@ const Qualification = () => {
 
                        <div>
                        <h3 className="qualification__title">BSc Computer Science{"(Incomplete)"}</h3>
-                        <span className="qualification__subitile">University of Witwatersrand</span>
-                        <div className="qualification__calender">
+                        <span className="qualification__subtitle">University of Witwatersrand</span>
+                        <div className="qualification__calendar">
                             <i className="uil uil-calendar-alt"></i> 2018 - 2021(3rd year)
                         </div>
                        </div>
@@ -63,9 +75,9 @@ const Qualification = () => {
                     <div className="qualification__data">
                        <div>
                        <h3 className="qualification__title">National Senior Certificate</h3>
-                        <span className="qualification__subitile">Orlando West High</span>
-                        <div className="qualification__calender">
-                            <i className="uil uil-calendar-alt"></i> 2014 - 2017(Complete)
+                        <span className="qualification__subtitle">Orlando West High</span>
+                        <div className="qualification__calendar">
+                            <i className="uil uil-calendar-alt"></i> 2014 - 2017
                         </div>
                        </div>
 
@@ -80,13 +92,14 @@ const Qualification = () => {
 
                 </div>
 
-                <div className="qualification__content">
+                <div className={toggleState === 2 ? "qualification__content qualification__content-active":
+                    "qualification__content"}>
                     <div className="qualification__data">
                        <div>
                        <h3 className="qualification__title">
                        Jnr Software Engineer Intern</h3>
-                        <span className="qualification__subitile">MyDough</span>
-                        <div className="qualification__calender">
+                        <span className="qualification__subtitle">MyDough</span>
+                        <div className="qualification__calendar">
                             <i className="uil uil-calendar-alt"></i> Feb 2024
                         </div>
                        </div>
@@ -110,8 +123,8 @@ const Qualification = () => {
 
                        <div>
                        <h3 className="qualification__title">BootCamp Technical Mentor</h3>
-                        <span className="qualification__subitile">WeThinkCode_</span>
-                        <div className="qualification__calender">
+                        <span className="qualification__subtitle">WeThinkCode_</span>
+                        <div className="qualification__calendar">
                             <i className="uil uil-calendar-alt"></i> Apr 2023 - Aug 2023
 
                         </div>
@@ -122,8 +135,8 @@ const Qualification = () => {
                     <div className="qualification__data">
                        <div>
                        <h3 className="qualification__title">BootCamp Project Reviewer</h3>
-                        <span className="qualification__subitile">WeThinkCode_</span>
-                        <div className="qualification__calender">
+                        <span className="qualification__subtitle">WeThinkCode_</span>
+                        <div className="qualification__calendar">
                             <i className="uil uil-calendar-alt"></i> Apr 2023 - Aug 2023
                         </div>
                        </div>
