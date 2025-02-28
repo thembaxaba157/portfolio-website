df6f7c8990dc2a988b18436524a830cdde556d91
thembaxaba157 <thembaxaba157@gmail.com>
2025-02-28 17:22:57 +0200

    Check if your phone is reponsive

--- src/components/work/Works.jsx
+++ src/components/work/Works.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { projectsData } from './Data'
 import { projectsNav } from './Data'
 import WorkItems from './WorkItems'
@@ -6,20 +6,46 @@ import WorkItems from './WorkItems'
 
 
 const Works = () => {
+  const [item, setItem] = useState({ name: 'all' });
+  const [projects, setProjects] = useState([]);
+  const [active, setActive] = useState(0);
+
+  useEffect(() => {
+        if(item.name === "all") {
+          setProjects(projectsData);
+        }
+        else {
+          const newProjects = projectsData.filter((project) => {
+            return project.category === item.name;
+          });
+          setProjects(newProjects);
+        }
+  } , [item]);
+
+  const handleClick = (e, index) => {
+    setItem({ name: e.target.textContent});
+    setActive(index);
+  };
+
   return (
     <div>
-
       <div className="work__filters">
             {projectsNav.map((item, index) => {
-              return( <span className="work__item" key={index}>
-                  {item.name}
+              return( <span onClick={(e) => {
+                handleClick(e, index);
+              }}
+              className={`${active === index ? 'active-work' : ""}
+                work__item`}
+              key={index}>
+              {item.name}
+              
               </span>
               );
       })}
     </div>
 
       <div className="work__container container grid">
-        {projectsData.map((item) => {
+        {projects.map((item) => {
           return ( <WorkItems item={item} key={item.id} />)
         })}
       </div>
